914bb403737795c5ed8fddbf86665172a27256ec
niveditaece <niveditaece@yahoo.com>
2018-02-04 07:09:08 +0530

    clean up

--- src/app/book-create/book-create.component.ts
+++ src/app/book-create/book-create.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-book-create',
@@ -9,16 +12,21 @@ import { HttpClient } from '@angular/common/http';
   encapsulation: ViewEncapsulation.None
 })
 export class BookCreateComponent implements OnInit {
-
-  book = {};
-
-  constructor(private http: HttpClient, private router: Router) { }
+ private API_URL = "https://evening-temple-66423.herokuapp.com";
+ private cors_api_host = 'cors-anywhere.herokuapp.com';
+    private cors_api_url = 'https://' + this.cors_api_host + '/';
+  book: any = {};
+  
+ 
+  constructor(private http: Http,private router: Router) {console.log('[Bookcreatecomponent] '); }
 
   ngOnInit() {
   }
 
   saveBook() {
-  this.http.post('/book', this.book)
+console.log('[Bookcreatecomponent] ');
+return this.http
+      .post(this.cors_api_url+this.API_URL+'/book', this.book)
       .subscribe(res => {
           let id = res['_id'];
           this.router.navigate(['/book-details', id]);
@@ -26,6 +34,13 @@ export class BookCreateComponent implements OnInit {
           console.log(err);
         }
       );
+  
   }
+ 
 
+  
+
+ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
